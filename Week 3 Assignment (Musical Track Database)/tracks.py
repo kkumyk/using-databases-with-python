e8629e9dec4340ca0758463df4b802bb5f76fd60
Karina Kumykova <k.kumykova@gmail.com>
2022-02-15 14:04:57 +0000

    add updated tracks.py file

--- Week 3 Assignment (Musical Track Database)/tracks.py
+++ Week 3 Assignment (Musical Track Database)/tracks.py
@@ -1,16 +1,21 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 
-conn = sqlite3.connect('trackdb.sqlite')  # create DB if it does not exist
+conn = sqlite3.connect('tracksdb_updated.sqlite')  # create DB if it does not exist
 cur = conn.cursor()
 
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Track;
-
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -26,12 +31,15 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
 
+# Getting the data and parsing it
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'Library.xml'  # ask for the XML file
+if (len(fname) < 1):
+    fname = 'Library.xml'  # ask for the XML file
 
 
 # <key>Track ID</key><integer>369</integer>
@@ -41,7 +49,8 @@ if (len(fname) < 1): fname = 'Library.xml'  # ask for the XML file
 def lookup(d, key):
     found = False
     for child in d:
-        if found: return child.text
+        if found:
+            return child.text
         if child.tag == 'key' and child.text == key:
             found = True
     return None
@@ -55,31 +64,37 @@ for entry in all:
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
+    genre = lookup(entry, "Genre")
     album = lookup(entry, 'Album')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
 
-    if name is None or artist is None or album is None:
+    if name is None or artist is None or album is None or genre is None:
         continue
-
-    print(name, artist, album, count, rating, length)
+    print(name, artist, genre, album, count, rating, length)
 
     # OR IGNORE is a way to make sure that no duplicate entries are added
     # == if the artist's name is already there, don't insert is again
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', (artist,))
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist,))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', (genre,))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre,))
+    genre_id = cur.fetchone()[0]
+
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', (album, artist_id))
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album,))
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''',
-                (name, album_id, length, rating, count))
+        (title, album_id, len, rating, count, genre_id)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+                (name, album_id, length, rating, count, genre_id))
 
     conn.commit()
